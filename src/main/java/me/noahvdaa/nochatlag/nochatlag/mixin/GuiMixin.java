b9472bf20ef076765525225342e283c54797782c
Noah van der Aa <ndvdaa@gmail.com>
2022-01-07 18:42:14 +0100

    Add icon and remap to yarn

--- src/main/java/me/noahvdaa/nochatlag/nochatlag/mixin/GuiMixin.java
+++ src/main/java/me/noahvdaa/nochatlag/nochatlag/mixin/GuiMixin.java
@@ -1,10 +1,10 @@
 package me.noahvdaa.nochatlag.nochatlag.mixin;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.chat.ChatListener;
-import net.minecraft.network.chat.ChatType;
-import net.minecraft.network.chat.Component;
-import net.minecraft.util.HttpUtil;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.ClientChatListener;
+import net.minecraft.client.util.NetworkUtils;
+import net.minecraft.network.MessageType;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,33 +17,32 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-@Mixin(net.minecraft.client.gui.Gui.class)
-public class GuiMixin {
+@Mixin(net.minecraft.client.gui.hud.InGameHud.class)
+public abstract class GuiMixin {
 
 	@Final
 	@Shadow
-	private Minecraft minecraft;
+	private MinecraftClient client;
 
 	@Final
 	@Shadow
-	private Map<ChatType, List<ChatListener>> chatListeners;
+	private Map<MessageType, List<ClientChatListener>> listeners;
+
+	@Shadow
+	public abstract UUID extractSender(Text message);
 
 	@Inject(
-			method = "handleChat(Lnet/minecraft/network/chat/ChatType;Lnet/minecraft/network/chat/Component;Ljava/util/UUID;)V",
+			method = "addChatMessage(Lnet/minecraft/network/MessageType;Lnet/minecraft/text/Text;Ljava/util/UUID;)V",
 			at = @At("HEAD"),
 			cancellable = true
 	)
-	public void handleChat(ChatType chatType, Component chatComponent, UUID senderUUID, CallbackInfo ci) {
-		HttpUtil.DOWNLOAD_EXECUTOR.submit(() -> {
-			if (!minecraft.isBlocked(senderUUID)) {
-				if (!minecraft.options.hideMatchedNames || !minecraft.isBlocked(((GuiInvokerMixin) this).invokeGuessChatUUID(chatComponent))) {
-					Iterator chatListenerIterator = ((List) chatListeners.get(chatType)).iterator();
-
-					while (chatListenerIterator.hasNext()) {
-						ChatListener listener = (ChatListener) chatListenerIterator.next();
-						listener.handle(chatType, chatComponent, senderUUID);
-					}
-				}
+	public void handleChat(MessageType chatType, Text chatComponent, UUID senderUUID, CallbackInfo ci) {
+		NetworkUtils.EXECUTOR.submit(() -> {
+			if (this.client.shouldBlockMessages(senderUUID) || (this.client.options.hideMatchedNames && this.client.shouldBlockMessages(this.extractSender(chatComponent)))) {
+				return;
+			}
+			for (ClientChatListener chatListener : this.listeners.get(chatType)) {
+				chatListener.onChatMessage(chatType, chatComponent, senderUUID);
 			}
 		});
 		ci.cancel();
